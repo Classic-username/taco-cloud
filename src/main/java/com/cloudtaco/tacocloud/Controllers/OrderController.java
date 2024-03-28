3b353b6fac0d1390727e275c503cc61b0bb5cbf2
Classic-username <josh.drapergame@gmail.com>
2024-03-28 06:58:58 -0600

    Added comments for unused imports

--- src/main/java/com/cloudtaco/tacocloud/Controllers/OrderController.java
+++ src/main/java/com/cloudtaco/tacocloud/Controllers/OrderController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
-import com.cloudtaco.tacocloud.Domains.TacoOrder;
+import com.cloudtaco.tacocloud.Domains.TacoOrder; // the use of this import comes later, book said we'll be refactoring the code to store information in a database but for the time being we make it like this.
 
-@Slf4j
+@Slf4j // this supposedly happens at compile time
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
