2b4a7507367563af76f22f089658cd8eb7d725e1
Classic-username <josh.drapergame@gmail.com>
2024-03-28 18:52:28 -0600

    Added annotations to TacoOrder domain

--- src/main/java/com/cloudtaco/tacocloud/Domains/TacoOrder.java
+++ src/main/java/com/cloudtaco/tacocloud/Domains/TacoOrder.java
@@ -1,4 +1,8 @@
 package com.cloudtaco.tacocloud.Domains;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.CreditCardNumber;
 import java.util.List;
 import java.util.ArrayList;
 import lombok.Data;
@@ -6,14 +10,30 @@ import lombok.Data;
 @Data
 public class TacoOrder {
 
+    @NotBlank(message="Delivery name is required")
     private String deliveryName;
+
+    @NotBlank(message="Street is required")
     private String deliveryStreet;
+
+    @NotBlank(message="City is required")
     private String deliveryCity;
+
+    @NotBlank(message="State is required")
     private String deliveryState;
+
+    @NotBlank(message="Zip code is required")
     private String deliveryZip;
+
+    @CreditCardNumber(message="Not a valid credit card number")
     private String ccNumber;
+
+    @Pattern(regexp="^([1-9] |1[0-2]) ([\\/]) ([2-9] [0-9])$", message="Must be formatted MM/YY")
     private String ccExpiration;
+
+    @Digits(intiger=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
+
 
     private List<Taco> tacos = new ArrayList<>();
 
