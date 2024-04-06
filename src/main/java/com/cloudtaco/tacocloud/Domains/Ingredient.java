dfdccedb10a073f9213908ad3c3a140506fcaa57
Classic-username <josh.drapergame@gmail.com>
2024-04-06 09:14:55 -0600

    Got ahead of myself, now ingredient.java is prepped for data persistence

--- src/main/java/com/cloudtaco/tacocloud/Domains/Ingredient.java
+++ src/main/java/com/cloudtaco/tacocloud/Domains/Ingredient.java
@@ -1,22 +1,39 @@
 package com.cloudtaco.tacocloud.Domains;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Data
 @Table
-public class Ingredient {
+@AllArgsConstructor
+@NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
+public class Ingredient implements Persistable<String> {
     
     @Id
     private final String id;
 
     private final String name;
-    
+
     private final Type type;
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
+
+    @Override
+    public boolean isNew() {
+        throw new UnsupportedOperationException("Unimplemented method 'isNew'");
+    }
+    //This override is auto generated to get past YET MORE THINGS THIS BOOK DOES NOT EXPLAIN OR SHOW
+    //The book did not show this (as of page 83) but its implementation of this method is:
+    // @Override
+    // public boolean isNew() {
+    //     return true;
+    // }
 }
