795f58b7f82738687b0a827d91e417e24b652faa
Classic-username <josh.drapergame@gmail.com>
2024-04-06 06:15:52 -0600

    had to update a data type in Taco domain so jdbcOrderRepository would work

--- src/main/java/com/cloudtaco/tacocloud/Domains/Taco.java
+++ src/main/java/com/cloudtaco/tacocloud/Domains/Taco.java
@@ -1,5 +1,6 @@
 package com.cloudtaco.tacocloud.Domains;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Date;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -16,8 +17,8 @@ public class Taco {
     @NotNull
     @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
-    @NotNull
+
     @Size(min=1, message="You must choose at least one ingredient")
-    private List<Ingredient> ingredients;
+    private List<IngredientRef> ingredients;
     
 }
