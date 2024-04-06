d0ee90c76dfc0c9d43df506e7fa3d169ed70e29d
Classic-username <josh.drapergame@gmail.com>
2024-04-06 07:57:52 -0600

    Troubleshooting

--- src/main/java/com/cloudtaco/tacocloud/Repositories/JdbcOrderRepository.java
+++ src/main/java/com/cloudtaco/tacocloud/Repositories/JdbcOrderRepository.java
@@ -29,7 +29,7 @@ public class JdbcOrderRepository implements OrderRepository {
     @Transactional
     public TacoOrder save(TacoOrder order){
         PreparedStatementCreatorFactory pscf = new PreparedStatementCreatorFactory(
-            "insert into Taco_Order (delivery_name, delivery_street, delivery_city, delivery_state, delivery_zip, cc_number, cc_expiration, cc_cvv, placed_at) values (?,?,?,?,?,?,?,?,?)", Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.TIMESTAMP
+            "insert into Taco_Order (delivery_name, delivery_street, delivery_city, delivery_state, delivery_zip, cc_number, cc_expiration, cc_cvv, placed_at) values (?,?,?,?,?,?,?,?,?)", Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.TIMESTAMP
         );
         pscf.setReturnGeneratedKeys(true);
 
