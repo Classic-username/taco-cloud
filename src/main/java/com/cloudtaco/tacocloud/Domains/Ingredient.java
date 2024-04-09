d60300f6803a9a812e0e604da05c54ac2d640f94
Classic-username <josh.drapergame@gmail.com>
2024-04-09 05:27:05 -0600

    Updated ingredient domain object to use JPA instead of JDBC

--- src/main/java/com/cloudtaco/tacocloud/Domains/Ingredient.java
+++ src/main/java/com/cloudtaco/tacocloud/Domains/Ingredient.java
@@ -1,40 +1,27 @@
 package com.cloudtaco.tacocloud.Domains;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.domain.Persistable;
-import org.springframework.data.relational.core.mapping.Table;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import lombok.With;
 
 @Data
-@Table
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
-public class Ingredient implements Persistable<String> {
+public class Ingredient{
     
     @Id
-    private @With final String id;
+    private String id;
 
-    private @With final String name;
+    private String name;
 
-    private @With final Type type;
+    private Type type;
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
-
-    // @Override
-    // public boolean isNew() {
-    //     throw new UnsupportedOperationException("Unimplemented method 'isNew'");
-    // }
-    //This override is auto generated to get past YET MORE THINGS THIS BOOK DOES NOT EXPLAIN OR SHOW
-    //The book did not show this (as of page 83) but its implementation of this method is:
-    @Override
-    public boolean isNew() {
-        return true;
-    }
+    
 }
