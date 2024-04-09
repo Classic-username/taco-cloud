eacc32b7c65cbb1f07c3232eb61e8668ab805de9
Classic-username <josh.drapergame@gmail.com>
2024-04-09 05:44:04 -0600

    Removed some comments

--- src/main/java/com/cloudtaco/tacocloud/Repositories/IngredientRepository.java
+++ src/main/java/com/cloudtaco/tacocloud/Repositories/IngredientRepository.java
@@ -2,11 +2,8 @@ package com.cloudtaco.tacocloud.Repositories;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
-// import org.springframework.data.repository.Repository;
 import com.cloudtaco.tacocloud.Domains.Ingredient;
 
-//This Repository will use the first implementation shown, the OrderRepository will use the second.
-//Repository doesn't have the deleteAll() method being called in the commandlinerunner in app.java, so I had to change to crudrepository
 public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 
     Iterable<Ingredient> findAll();
