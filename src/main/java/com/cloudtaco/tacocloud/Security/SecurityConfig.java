f792493d89dbe6fcd5a5749fff594abb016c3c3d
Classic-username <josh.drapergame@gmail.com>
2024-04-20 08:40:11 -0600

    updated securityconfig to use actual users and not pre-written users

--- src/main/java/com/cloudtaco/tacocloud/Security/SecurityConfig.java
+++ src/main/java/com/cloudtaco/tacocloud/Security/SecurityConfig.java
@@ -8,9 +8,12 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import com.cloudtaco.tacocloud.Repositories.UserRepository;
 
 @Configuration
 public class SecurityConfig {
@@ -20,10 +23,20 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        List<UserDetails> usersList = new ArrayList<>();
-        usersList.add(new User("buzz",encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
-        usersList.add(new User("woody",encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
-        return new InMemoryUserDetailsManager(usersList);
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+        return username -> {
+            com.cloudtaco.tacocloud.Domains.User user = userRepo.findByUsername(username);
+            if (user != null) return user;
+
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        };
     }
+
+    // @Bean
+    // public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+    //     List<UserDetails> usersList = new ArrayList<>();
+    //     usersList.add(new User("buzz",encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+    //     usersList.add(new User("woody",encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+    //     return new InMemoryUserDetailsManager(usersList);
+    // }
 }
