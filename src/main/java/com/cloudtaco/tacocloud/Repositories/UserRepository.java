7e63cd12f2ba30316c38ea5df149ff60bea1df2f
Classic-username <josh.drapergame@gmail.com>
2024-04-23 05:23:46 -0600

    added user repository

---
+++ src/main/java/com/cloudtaco/tacocloud/Repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.cloudtaco.tacocloud.Repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.cloudtaco.tacocloud.Domains.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
