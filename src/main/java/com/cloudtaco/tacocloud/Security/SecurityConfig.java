bdeb31894b433447cdcb516937701b338fe17928
Classic-username <josh.drapergame@gmail.com>
2024-04-23 06:00:59 -0600

    updated SecurityConfig with new spring security formatting

--- src/main/java/com/cloudtaco/tacocloud/Security/SecurityConfig.java
+++ src/main/java/com/cloudtaco/tacocloud/Security/SecurityConfig.java
@@ -6,13 +6,18 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.cloudtaco.tacocloud.Domains.User;
+import com.cloudtaco.tacocloud.Repositories.UserRepository;
 
 @Configuration
 public class SecurityConfig {
@@ -22,14 +27,23 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        List<UserDetails> usersList = new ArrayList<>();
-        usersList.add(new User(
-                "buzz", encoder.encode("password"),
-                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
-        usersList.add(new User(
-                "woody", encoder.encode("password"),
-                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
-        return new InMemoryUserDetailsManager(usersList);
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+        return username -> {
+            User user = userRepo.findByUsername(username);
+            if (user != null)
+                return user;
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        };
+    }
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+            .authorizeHttpRequests((authorize) -> authorize
+                .requestMatchers("/design", "/orders").hasRole("USER")
+                .requestMatchers("/", "/**")
+                .permitAll()
+            )
+            .build();
     }
 }
