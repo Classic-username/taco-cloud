41efc64109d2784703a212eca0c560d2a89e4a44
Classic-username <josh.drapergame@gmail.com>
2024-04-24 05:26:39 -0600

    attempted fix with mvc.pattern, didn't work.

--- src/main/java/com/cloudtaco/tacocloud/Security/SecurityConfig.java
+++ src/main/java/com/cloudtaco/tacocloud/Security/SecurityConfig.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +16,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import com.cloudtaco.tacocloud.Domains.User;
 import com.cloudtaco.tacocloud.Repositories.UserRepository;
@@ -35,18 +38,26 @@ public class SecurityConfig {
             throw new UsernameNotFoundException("User '" + username + "' not found");
         };
     }
+
+    @Bean
+    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+        return new MvcRequestMatcher.Builder(introspector);
+    }
+
+    @Autowired
+    MvcRequestMatcher.Builder mvc;
     
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
             .authorizeHttpRequests((authorize) -> authorize
-                .requestMatchers("/design", "/orders").hasRole("USER")
-                .requestMatchers("/", "/**")
+                .requestMatchers(mvc.pattern("/design"), mvc.pattern("/orders")).hasRole("USER")
+                .requestMatchers(mvc.pattern("/"), mvc.pattern("/**"))
                 .permitAll()
             )
             .formLogin(formLogin -> formLogin
                 .loginPage("/login")
-                .defaultSuccessUrl("/", true)
+                .defaultSuccessUrl("/design", true)
             )
             //logging out...
             //.rememberMe(rememberMe -> rememberMe.key("seeminglyrandomstring...")).logout(logout -> logout.logoutUrl("/signout").permitAll())
