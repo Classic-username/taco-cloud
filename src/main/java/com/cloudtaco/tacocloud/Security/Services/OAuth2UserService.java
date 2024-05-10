d0a0439940dc7241eb04934ab659a928e2e9f696
Classic-username <josh.drapergame@gmail.com>
2024-05-10 05:15:02 -0600

    Created an OAuth2UserService file, it has several imported files we don't have created so we'll be moving on in the book.

---
+++ src/main/java/com/cloudtaco/tacocloud/Security/Services/OAuth2UserService.java
@@ -0,0 +1,16 @@
+// package com.cloudtaco.tacocloud.Security.Services;
+
+// import lombok.RequiredArgsConstructor;
+// import lombok.SneakyThrows;
+// import lombok.extern.slf4j.Slf4j;
+// import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+// import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+// import org.springframework.security.oauth2.core.user.OAuth2User;
+// import org.springframework.stereotype.Service;
+
+// import java.util.Optional;
+// import java.util.UUID;
+
+// public class OAuth2UserService extends DefaultOAuth2UserService {
+    
+// }
