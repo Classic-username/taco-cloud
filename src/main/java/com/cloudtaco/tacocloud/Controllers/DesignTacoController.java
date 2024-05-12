3b3e0d04ebad1e23b57d12f2f2622a4a30e3d4c0
Classic-username <josh.drapergame@gmail.com>
2024-05-12 08:36:15 -0600

    re-refactored design controller for submission

--- src/main/java/com/cloudtaco/tacocloud/Controllers/DesignTacoController.java
+++ src/main/java/com/cloudtaco/tacocloud/Controllers/DesignTacoController.java
@@ -2,14 +2,12 @@ package com.cloudtaco.tacocloud.Controllers;
 
 import jakarta.validation.Valid;
 
-import java.security.Principal;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.ui.Model;
@@ -24,6 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 import com.cloudtaco.tacocloud.Domains.Ingredient;
 import com.cloudtaco.tacocloud.Domains.Ingredient.Type;
 import com.cloudtaco.tacocloud.Repositories.IngredientRepository;
+import com.cloudtaco.tacocloud.Repositories.OrderRepository;
 import com.cloudtaco.tacocloud.Repositories.TacoRepository;
 import com.cloudtaco.tacocloud.Repositories.UserRepository;
 import com.cloudtaco.tacocloud.Domains.Taco;
@@ -38,28 +37,44 @@ public class DesignTacoController {
 
     private final IngredientRepository ingredientRepo;
 
-    private TacoRepository tacoRepo;
-
-    private UserRepository userRepo;
+    private final OrderRepository orderRepo;
 
     @Autowired
-    public DesignTacoController(IngredientRepository ingredientRepo, TacoRepository tacoRepo, UserRepository userRepo){
+    public DesignTacoController(IngredientRepository ingredientRepo, OrderRepository orderRepo){
         this.ingredientRepo = ingredientRepo;
-        this.tacoRepo = tacoRepo;
-        this.userRepo = userRepo;
+        this.orderRepo = orderRepo;
     }
 
     @ModelAttribute
     public void addIngredientsToModel(Model model) {
         
-        List<Ingredient> ingredients = new ArrayList();
-        ingredientRepo.findAll().forEach(i -> ingredients.add(i));
-
+        Iterable<Ingredient> ingredients = ingredientRepo.findAll();
         Type[] types = Ingredient.Type.values();
         for (Type type: types) {
             model.addAttribute(type.toString().toLowerCase(),
             filterByType(ingredients, type));
         }
+
+        // The below has been turned into a DB query
+        // List<Ingredient> ingredients = Arrays.asList(
+        //     new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+        //     new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+        //     new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+        //     new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+        //     new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+        //     new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+        //     new Ingredient("CHED", "Cheddar", Type.CHEESE),
+        //     new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+        //     new Ingredient("SLSA", "Salsa", Type.SAUCE),
+        //     new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+        // );
+
+        // Type[] types = Ingredient.Type.values();
+
+        // for (Type type : types) {
+        //     model.addAttribute(type.toString().toLowerCase(),
+        //     filterByType(ingredients, type));
+        // }
     }
 
     @ModelAttribute(name = "tacoOrder")
@@ -72,38 +87,28 @@ public class DesignTacoController {
         return new Taco();
     }
 
-    @ModelAttribute(name = "user")
-    public Users user(Principal principal) {
-        String username = principal.getName();
-        Users user = userRepo.findByUsername(username);
-        return user;
-    }
-
     @GetMapping
     public String showDesignForm() {
         return "design";
     }
-    
-    @PostMapping
-    public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
 
-        log.info("Processing taco: {}", taco);
-        
+    private Iterable<Ingredient> filterByType(Iterable<Ingredient> ingredients, Type type) {
+        return StreamSupport.stream(ingredients.spliterator(), false)// The book did NOT tell us this part (at least by page 69 when we refactor this class)
+        .filter(x -> x.getType().equals(type))
+        .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder, @AuthenticationPrincipal Users user) {
+
         if(errors.hasErrors()) {
             return "design";
         }
         
-        Taco saved = tacoRepo.save(taco);
-        tacoOrder.addTaco(saved);
-        
+        tacoOrder.addTaco(taco);
+        tacoOrder.setUser(user);
+        log.info("Processing taco: {}", taco);
+    
         return "redirect:/orders/current";
     }
-    
-        private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
-            return ingredients
-            .stream()
-            .filter(x -> x.getType().equals(type))
-            .collect(Collectors.toList());
-        }
-    
 }
